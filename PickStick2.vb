018c79d66449276f3db3dba8acd8b3a14f6fca22
snarfor <snarfor@live.com>
2015-10-12 21:00:50 -0700

    Create PickStick2.vb

---
+++ PickStick2.vb
@@ -0,0 +1,68 @@
+Option Strict On
+Public Class frmPickUpSticksBeta
+Private Sub frmPickUpSticksAlpha_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+	Randomize()
+	Dim c As Integer = CInt(10 + 40 * Rnd())
+	Dim str As New String(CChar("|"), c)
+	Dim A As Integer = Len(str)
+	NewGame(str, A)
+End Sub
+' This function initializes the pile
+Sub NewGame(ByVal str As String, ByVal a As Integer)
+	txtPile.Text = str
+	lblCounter.Text = CStr(a)
+End Sub
+Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+	YourMove(2)
+End Sub
+Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+	YourMove(3)
+End Sub
+Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+	YourMove(4)
+End Sub
+' This function implements your move of n sticks.
+Sub YourMove(ByVal n As Integer)
+	MessageBox.Show("You decided to pick " & n & " sticks", "Your Move")
+	Dim D As Integer
+	D = CInt(lblCounter.Text) - CInt(n)
+	If D > 0 Then
+		lblCounter.Text = CStr(D)
+		Dim H As New String(CChar("|"), D)
+		txtPile.Text = H
+		ComputerMove(n)
+		Return
+	Else
+		MessageBox.Show("You lost", "Your Move")
+		Randomize()
+		Dim str As New String(CChar("|"), CInt(10 + 40 * Rnd()))
+		Dim A As Integer = Len(str)
+		NewGame(str, A)
+	End If
+End Sub
+' This function implements computer's response to your move of n sticks
+Sub ComputerMove(ByVal n As Integer)
+	Threading.Thread.Sleep(100)
+	Randomize()
+	Dim D As Integer
+	D = CInt(lblCounter.Text) - CInt(2 + 2 * Rnd())
+	If D > 0 Then
+		MessageBox.Show("Computer decided to pick " & CInt(lblCounter.Text) - D & " sticks", "Your Move")
+		lblCounter.Text = CStr(D)
+		Dim H As New String(CChar("|"), D)
+		txtPile.Text = H
+	Else
+		MessageBox.Show("Congratulations, you win!", "Computermove")
+		Randomize()
+		Dim str As New String(CChar("|"), CInt(10 + 40 * Rnd()))
+		Dim A As Integer = Len(str)
+		NewGame(str, A)
+	End If
+End Sub
+Private Function n() As Integer
+	Throw New NotImplementedException
+End Function
+&#12288;
+&#12288;
+&#12288;
+End Class
